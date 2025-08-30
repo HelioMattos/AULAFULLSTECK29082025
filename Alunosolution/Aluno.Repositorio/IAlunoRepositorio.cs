263b3fe5a40024aae17038d4201b9a17552bb691
Helio Mattos <heliomatto@gmail.com>
2025-08-29 21:20:46 -0300

    imlementacao do repositorio

---
+++ Alunosolution/Aluno.Repositorio/IAlunoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using Aluno.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno.Repositorio
+{
+    public interface IAlunoRepositorio
+    {
+        void Adicionar();
+        List<int> Listar();
+    }
+}
+    
